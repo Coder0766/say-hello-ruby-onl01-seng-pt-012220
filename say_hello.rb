977dae041325a840a15fc9eb1ba57ebee0a917b5
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-01-23 19:32:11 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_he!llo method here
-def say_hello(name = "Ruby programmer!")
-  puts "Hello, #{name!}"
+def say_hello("name" = "Ruby programmer!")
+  puts "Hello, #{"name!"}"
 end
 
 say_hello("Courtney")
